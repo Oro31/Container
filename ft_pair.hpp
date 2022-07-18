cf357f19276b2f93afd3d69a4b94ded015f359c8
Oro31 <60878633+Oro31@users.noreply.github.com>
2022-07-18 07:28:22 +0200

    add pair

---
+++ ft_pair.hpp
@@ -0,0 +1,15 @@
+#include <memory>
+
+namespace ft {
+	template <class T1, class T2>
+	class Pair {
+		public:
+			typedef T1	first_type;
+			typedef T2	second_type;
+
+		protected:
+		private:
+			first_type	first;
+			second_type	second;
+	}
+}
