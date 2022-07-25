cb0ac2f2bb772919cecac39c4051910dd76dad2c
Oro31 <60878633+Oro31@users.noreply.github.com>
2022-07-25 16:15:06 +0200

    iterator in progress

--- ft_iterator.hpp
+++ ft_iterator.hpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <iterator>
 
-class MyIterator : public std::iterator<std::input_iterator_tag, int> {
-		int	*p;
+class MyIterator : public std::iterator<std::random_acces_iterator_tag, T, Distance = std::ptrdiff_t, Pointer = T*, Reference = T&> {
+		T	*p;
 	public:
-		MyIterator(int *x) : p(x) {}
+		MyIterator(T *x) : p(x) {}
 		MyIterator(const MyIterator &mit) : p(mit.p) {}
 		MyIterator	&operator++() {++p; return *this;}
-		MyIterator	operator++(int) {
+		MyIterator	operator++(T) {
 			MyIterator tmp(*this); operator++(); return tmp;
 		}
 		bool	operator==(const MyIterator &rhs) const {return p==rhs.p;}
 		bool	operator!=(const MyIterator &rhs) const {return p!=rhs.p;}
-		int		&operator*() {return *p;}
+		T		&operator*() {return *p;}
 }
