be8a927964dbbf1ee6853f595047ce2a970a5cf1
Oro31 <60878633+Oro31@users.noreply.github.com>
2022-09-09 06:16:27 +0200

    compilation succes

--- first/vector.hpp
+++ first/vector.hpp
@@ -28,8 +28,10 @@ namespace ft {
 			iterator	end_of_buffer;
 
 		public:
+
 			//
 			//construct/copy/destroy
+			//
 			//
 			explicit vector(const allocator_type &alloc = allocator_type()) :
 				buffer_start(0), current_end(0), end_of_buffer(0) {};
@@ -78,7 +80,7 @@ namespace ft {
 			//
 			vector &operator=(const vector<T, Allocator> &other) {
 				iterator	first = other.begin();
-				size_t	buf_size = other.capacity();
+				size_type	buf_size = other.capacity();
 				the_allocator = other.get_allocator();
 				buffer_start = the_allocator.allocate(buf_size);
 				end_of_buffer = buffer_start + buf_size;
@@ -90,7 +92,7 @@ namespace ft {
 			//
 			template<class InputIt>
 				void assign(InputIt first, InputIt last) {
-					if (last - first > this->capacity()) {this->reserve(last - first)}
+					if (last - first > this->capacity()) {this->reserve(last - first);}
 					for (iterator it = buffer_start; it != current_end; it++) {
 						it->~value_type();
 					}
@@ -102,7 +104,7 @@ namespace ft {
 				};
 			//
 			void assign(size_type n, const T &value) {
-				if (n > this->capacity()) {this->reserve(n)}
+				if (n > this->capacity()) {this->reserve(n);}
 				for (iterator it = buffer_start; it != current_end; it++) {
 					it->~value_type();
 				}
@@ -133,7 +135,9 @@ namespace ft {
 			//
 			
 			size_type	size() const {return current_end - buffer_start;};
+			//
 			size_type	max_size() const {return the_allocator.max_size();};
+			//
 			void		resize(size_type sz, T c = T()) {
 				if (sz <= this->size()) {
 					for (size_type i = this->size(); i > sz; i--) {
@@ -144,23 +148,28 @@ namespace ft {
 					if (sz > this->capacity()) {
 						this->reserve(sz);
 					}
+					for (iterator last = buffer_start + sz; current_end != last; current_end++) {
+						the_allocator.construct(current_end.base(), c);
+					}
 				}
 			};
-			size_type capacity() {return end_of_buffer - buffer_start;};
+			//
+			size_type capacity() const {return end_of_buffer - buffer_start;};
+			//
 			bool empty() {return !(current_end - buffer_start);};
+			//
 			void reserve(size_type new_cap) {
 				if (new_cap > this->capacity()) {
 					T	p(*this);
 					this->~vector();
-					buffer_start = the_allocator.allocate(new_cap * 2);
+					buffer_start = the_allocator.allocate((this->capacity() + 1) * new_cap * 2);
 					end_of_buffer = buffer_start + (new_cap * 2);
 					current_end = buffer_start;
-					for (iterator it = p.buffer_start; it != p.current_end; it++ current_end++) {
+					for (iterator it = p.buffer_start; it != p.current_end; it++, current_end++) {
 						the_allocator.construct(current_end.base(), *it);
 					}
 				}
 			};
-
 
 			//
 			//element acces//
@@ -169,30 +178,37 @@ namespace ft {
 			reference operator[] (size_type pos) {
 				return *(buffer_start + pos);
 			};
+			//
 			const_reference operator[] (size_type pos) const {
 				return *(buffer_start + pos);
 			};
+			//
 			reference at(size_type pos) {
 				if ((buffer_start + pos).base() - current_end.base() >= 0) {
 					throw std::out_of_range("out of range");
 				}
 				return *(buffer_start + pos);
 			};
+			//
 			const_reference at(size_type pos) const {
 				if ((buffer_start + pos).base() - current_end.base() >= 0) {
 					throw std::out_of_range("out of range");
 				}
 				return *(buffer_start + pos);
 			};
+			//
 			reference	front() {
 				return *(buffer_start);
 			};
+			//
 			const_reference	front() const {
 				return *(buffer_start);
 			};
+			//
 			reference	back() {
 				return *(current_end - 1);
 			};
+			//
 			const_reference	back() const {
 				return *(current_end - 1);
 			};
@@ -202,16 +218,52 @@ namespace ft {
 			//
 			
 			void	push_back(const T &x) {
+				if (this->size() == this->capacity) {this->reserve(1);}
+				*current_end = x;
+				current_end++;
 			};
+			//
 			void	pop_back() {
-				iterator	it = current_end - 1;
-				it->~value_type();
+				current_end--;
+				*current_end->~value_type();
 			};
+			//
 			iterator	insert(iterator position, const T &x) {
+				size_type	pos = position - buffer_start;
+				T	p(*this);
+				if (this->size() == this->capacity) {this->reserve(1);}
+				(buffer_start + (pos - 1))->~value_type();
+				the_allocator.construct((buffer_start + (pos - 1)).base(), x);
+				current_end++;
+				for (iterator it = buffer_start + pos; it != current_end; it++) {
+					*it = *(p.buffer_start + (pos - 1));
+					p.buffer_start++;
+				}
+				return (buffer_start + (pos - 1));
 			};
+			//
 			template<class InputIt>
 				void	insert(iterator position, InputIt first, InputIt last) {
-			};
+					size_type	pos = position - buffer_start;
+					T	p(*this);
+					if (this->size() + (last - first) >= this->capacity) {this->reserve(last - first);}
+					(buffer_start + (pos - 1))->~value_type();
+					p.buffer_start = p.buffer_start + ((position - buffer_start) - 1);
+					(buffer_start + (pos - 1))->~value_type();
+					size_type	i = 0;
+					while (first != last) {
+						*(buffer_start + (pos + (i - 1))) = *first;
+						i++;
+						first++;
+					}
+					current_end = current_end + (last - first);
+					size_type	r = p.size() - pos;
+					for (size_type j = 0; j < r; j++ ) {
+						*current_end = *(p.buffer_start + (pos + j));
+						position++;
+					}
+				};
+			//
 			iterator	erase(iterator position) {
 				if (position < buffer_start || position >= current_end) {
 					return buffer_start;
@@ -226,6 +278,7 @@ namespace ft {
 				current_end--;
 				return (position + 1);
 			};
+			//
 			iterator	erase(iterator first, iterator last) {
 				if (last < buffer_start || first > current_end) {
 					return (current_end - 1);
@@ -250,6 +303,7 @@ namespace ft {
 				current_end = current_end - n;
 				return (first + n);
 			};
+			//
 			void	swap(vector<T, Allocator> &other) {
 				iterator	old_begin = buffer_start;
 				iterator	old_end = current_end;
@@ -262,20 +316,25 @@ namespace ft {
 				other.current_end = old_end;
 				other.end_of_buffer = old_end_of_buf;
 			};
+			//
 			void	clear() {
 				for (iterator it = this->end(); it != buffer_start; it--) {
 					it->~value_type();
 					current_end--;
 				}
-				it->~value_type();
+				buffer_start->~value_type();
 			};
 	};
+
+	//
+	//non-member operator
+	//
 
 	template <class T, class Allocator>
 		bool	operator==(const vector<T, Allocator> &x,
 					const vector<T, Allocator> &y) {
-			iterator	itx = x.begin();
-			iterator	ity = y.begin();
+			VecIterator<T>	itx = x.begin();
+			VecIterator<T>	ity = y.begin();
 			while (itx != x.end() && ity != y.end()) {
 				if (*itx != *ity) {
 					return false;
@@ -284,11 +343,12 @@ namespace ft {
 			}
 			return true;
 		};
+	//
 	template <class T, class Allocator>
 		bool	operator<(const vector<T, Allocator> &x,
 					const vector<T, Allocator> &y) {
-			iterator	itx = x.begin();
-			iterator	ity = y.begin();
+			VecIterator<T>	itx = x.begin();
+			VecIterator<T>	ity = y.begin();
 			while (itx != x.end() && ity != y.end()) {
 				if (*itx >= *ity) {
 					return false;
@@ -297,25 +357,28 @@ namespace ft {
 			}
 			return true;
 		};
+	//
 	template <class T, class Allocator>
 		bool	operator!=(const vector<T, Allocator> &x,
 					const vector<T, Allocator> &y) {
 			return !(x == y);
 		};
+	//
 	template <class T, class Allocator>
 		bool	operator>(const vector<T, Allocator> &x,
 					const vector<T, Allocator> &y) {
 			return (!(x < y) && x != y);
 		};
+	//
 	template <class T, class Allocator>
 		bool	operator>=(const vector<T, Allocator> &x,
 					const vector<T, Allocator> &y) {
 			return !(x < y);
 		};
+	//
 	template <class T, class Allocator>
 		bool	operator<=(const vector<T, Allocator> &x,
 					const vector<T, Allocator> &y) {
 			return !(x > y);
 		};
-
 }
