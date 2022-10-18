f42b66c1ac5ab9cd72158d67713ec8fb977ab4c4
Oro31 <60878633+Oro31@users.noreply.github.com>
2022-10-19 01:15:14 +0200

    vegeto

---
+++ sixth/VecIterator.hpp
@@ -0,0 +1,102 @@
+#include <memory>
+#include "utils.hpp"
+#include "iterator_traits.hpp"
+
+namespace ft {
+	template<class T>
+	class VecIterator {
+		public:
+			typedef typename ft::iterator_traits<T*>::value_type		value_type;
+			typedef typename ft::iterator_traits<T*>::pointer			pointer;
+			typedef typename ft::iterator_traits<T*>::reference			reference;
+			typedef typename ft::iterator_traits<T*>::difference_type	difference_type;
+			typedef std::random_access_iterator_tag						iterator_category;
+
+		private:
+			pointer	p;
+
+		public:
+			VecIterator() : p(0) {};
+			explicit VecIterator(pointer x) : p(x) {};
+			~VecIterator() {};
+
+			VecIterator &operator=(const pointer it) {p = it; return *this;};
+			VecIterator &operator=(const VecIterator &mit) {
+				p = mit.base();
+				return *this;
+			};
+			VecIterator(const VecIterator<typename std::remove_const<T>::type> &mit) : p(mit.base()) {};
+//			VecIterator(const VecIterator<const value_type> &mit) {*this = mit.base();};
+
+			reference operator*() {return *p;};
+			reference operator*() const {return *p;};
+			pointer operator->() {return p;};
+			pointer operator->() const {return p;};
+			pointer base() {return p;};
+			pointer base() const {return p;};
+
+
+			VecIterator &operator++() {++p; return *this;};
+			VecIterator &operator--() {--p; return *this;};
+			VecIterator operator++(int) {VecIterator tmp(*this); ++p; return tmp;};
+			VecIterator operator--(int) {VecIterator tmp(*this); --p; return tmp;};
+
+			VecIterator &operator+=(difference_type n) {
+				for (int i = 0; i < n; i++) {
+					++p;
+				}
+				return *this;
+			};
+			VecIterator &operator-=(difference_type n) {
+				for (int i = 0; i < n; i++) {
+					--p;
+				}
+				return *this;
+			};
+
+			reference operator[](difference_type n) {return *(p + n);};
+			reference operator[](difference_type n) const {return *(p + n);};
+
+			VecIterator operator+(difference_type n) const {VecIterator r(*this); r+=n; return r;};
+			VecIterator operator-(difference_type n) const {VecIterator r(*this); r-=n; return r;};
+
+			template <class Iterator>
+			difference_type operator-(const VecIterator<Iterator> &second) const {
+				return (base() - second.base());
+			};
+
+			template <class Iterator>
+				bool operator==(const VecIterator<Iterator> &other) const {return base() == other.base();};
+
+			template <class Iterator>
+				bool operator<(const VecIterator<Iterator> &other) const {return base() < other.base();};
+
+			template <class Iterator>
+				bool operator!=(const VecIterator<Iterator> &other) const {return base() != other.base();};
+
+			template<class Iterator>
+				bool operator>(const VecIterator<Iterator> &other) const {return base() > other.base();};
+
+			template<class Iterator>
+				bool operator<=(const VecIterator<Iterator> &other) const {return base() <= other.base();};
+
+			template<class Iterator>
+				bool operator>=(const VecIterator<Iterator> &other) const {return base() >= other.base();};
+
+	};
+
+	template<class Iterator>
+		Iterator operator+(const typename Iterator::difference_type &n, const Iterator &it) {
+			Iterator r(it);
+			r+=n;
+			return r;
+		};
+
+	template<class Iterator>
+		Iterator operator-(const typename Iterator::difference_type &n, const Iterator &it) {
+			Iterator r(it);
+			r-=n;
+			return r;
+		};
+
+}
---
+++ sixth/utils.hpp
@@ -0,0 +1,112 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+namespace ft {
+	template <bool cond, typename result=void>
+		struct	enable_if {
+		};
+
+	template <typename result>
+		struct enable_if<true, result> {
+			typedef result	type;// = result;
+		};
+
+	template <typename T, T val> struct is_integral_base {
+		static const T	value = val;
+	};
+
+	typedef is_integral_base<bool, true>	true_type;
+	typedef is_integral_base<bool, false>	false_type;
+
+	template <typename T> struct is_integral : public false_type {};
+	template <> struct is_integral<bool> : true_type {};
+	template <> struct is_integral<char> : true_type {};
+	template <> struct is_integral<wchar_t> : true_type {};
+	template <> struct is_integral<signed char> : true_type {};
+	template <> struct is_integral<short int> : true_type {};
+	template <> struct is_integral<int> : true_type {};
+	template <> struct is_integral<long int> : true_type {};
+	template <> struct is_integral<long long int > : true_type {};
+	template <> struct is_integral<unsigned char> : true_type {};
+	template <> struct is_integral<unsigned short int> : true_type {};
+	template <> struct is_integral<unsigned int> : true_type {};
+	template <> struct is_integral<unsigned long int> : true_type {};
+	template <> struct is_integral<unsigned long long int> : true_type {};
+
+	template <class InputIterator1, class InputIterator2>
+		bool equal(InputIterator1 first1, InputIterator1 last1,
+				InputIterator2 first2) {
+			while (first1 != last1) {
+				if (!(*first1 == *first2))
+					return false;
+				++first1;
+				++first2;
+			}
+			return true;
+		};
+
+	/*
+	template <class InputIterator1, class InputIterator2, class BinaryPredicate>
+		bool equal(InputIterator1 first1, InputIterator1 last1,
+				InputIterator2 first2, BinaryPredicate pred) {
+			while (first1 != last1) {
+				if (!pred(*first1, *first2))
+					return false;
+				++first1;
+				++first2;
+			}
+			return true;
+		};
+	*/
+
+	template <class InputIterator1, class InputIterator2>
+		bool	lexicographical_compare(InputIterator1 first1,
+										InputIterator1 last1,
+										InputIterator2 first2,
+										InputIterator2 last2) {
+			for (; (first1 != last1) && (first2 != last2); ++first1, (void) ++first2) {
+				if (*first1 < *first2)
+					return true;
+				if (*first2 < *first1)
+					return false;
+			}
+			return (first1 == last1) && (first2 != last2);
+		}
+			/*while (first1 != last1) {
+				if (first2 == last2 || *first2 < *first1)
+					return false;
+				else if (*first1 < *first2)
+					return true;
+				++first1;
+				++first2;
+			}
+			return (first2 != last2);*/
+
+	template <class InputIterator1, class InputIterator2, class Compare>
+		bool	lexicographical_compare(InputIterator1 first1,
+										InputIterator1 last1,
+										InputIterator2 first2,
+										InputIterator2 last2,
+										Compare comp) {
+			for (; (first1 != last1) && (first2 != last2); ++first1, (void) ++first2) {
+				if (comp(*first1, *first2))
+					return true;
+				if (comp(*first2, *first1))
+					return false;
+			}
+			return (first1 == last1) && (first2 != last2);
+		}
+	/*
+			while (first1 != last1) {
+				if (first2 == last2 || comp(*first2, *first1))
+					return false;
+				else if (comp(*first1, *first2))
+					return true;
+				++first1;
+				++first2;
+			}
+			return (first2 != last2);
+			*/
+};
+
+#endif
